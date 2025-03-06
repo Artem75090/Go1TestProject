4c0168aa8d13118d13316561b9eb1a3d0d18768d
sheve <shevelev67rf@yandex.ru>
2025-03-06 17:27:57 +0300

    пробный 3

--- main.go
+++ main.go
@@ -1,45 +1,9 @@
 package main
 
-import "fmt"
-
 type inter interface {
 	Metod()
 }
-type Human struct {
-	name string
-	age  int
-}
 
-func (h Human) Metod() {
-	fmt.Println("я человек", h.name, h.age)
-}
-func (h Human) Run() {
-	fmt.Println("я ПОБЕЖАЛ")
-}
-
-type Dag struct {
-	name string
-	age  int
-}
-
-func (d Dag) Metod() {
-	fmt.Println("я УТКА", d.name, d.age)
-}
-func (d Dag) Run1() {
-	fmt.Println("я плыву!")
-}
-func Res(i inter) {
-	i.Metod()
-	switch v := i.(type) {
-	case Human:
-		v.Run()
-	case Dag:
-		v.Run1()
-	}
-}
 func main() {
-	asd := Human{"max", 15}
-	zxc := Dag{"gaga", 10}
-	Res(asd)
-	Res(zxc)
+
 }
